bb82f51130badfb33a7694ec4a72399b2a9c2d5a
Fer Hernandez <fh.1111@gmail.com>
2019-01-03 12:59:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+     enable :sessions
+     set :session_secret, 'test'
+  end
+
+  get '/' do
+    erb :index
+  end  
 end
